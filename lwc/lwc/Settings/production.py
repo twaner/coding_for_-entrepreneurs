e128f4a485de3bcd4951a4543ce5a267d19db24d
t.waner <t.waner@gmail.com>
2015-03-07 19:03:42 -0500

    init

--- lwc/lwc/Settings/production.py
+++ lwc/lwc/Settings/production.py
@@ -1,14 +1,15 @@
 __author__ = 'taiowawaner'
-from django.conf import settings
-import dj_database_url
 import os
+from django.conf import settings
 
 DEBUG = False
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
+
 DATABASES = settings.DATABASES
 
 # Parse database configuration from $DATABASE_URL
-DATABASES['default'] = dj_database_url.config()
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -16,11 +17,14 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
+
+SHARE_URL = "http://launchwithcode.com/?ref="
 # Static asset configuration
+# import os
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # STATIC_ROOT = 'staticfiles'
 # STATIC_URL = '/static/'
-#
+
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
